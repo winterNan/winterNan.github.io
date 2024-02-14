a9664d137fc6a34e286c0c82939c4c93115da300
Yuan Yao <yuan.yao@it.uu.se>
2024-02-14 16:36:29 +0100

    update cal

---
+++ auto.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~/.emacs.d/org-agenda/
+./getUUCal.sh
+
+# check emacs server starts here
+
+emacsclient -e '(progn (org-batch-agenda "a") (org-agenda-write "/home/yuan/winterNan.github.io/index.html"))'
+cd /home/yuan/winterNan.github.io
+git add --all
+git commit -m "update cal"
+git push
