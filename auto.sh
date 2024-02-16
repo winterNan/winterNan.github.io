30d4e6e08060d3466b01f0ecc6ec8d6353d252b0
Yuan Yao <yuan.yao@it.uu.se>
2024-02-16 14:36:44 +0100

    update cal

--- auto.sh
+++ auto.sh
@@ -7,9 +7,6 @@ cd ~/.emacs.d/org-agenda/
 
 emacsclient -a false -e '(progn (org-batch-agenda "a") (org-agenda-write "/home/yuan/winterNan.github.io/index.html"))'
 cd /home/yuan/winterNan.github.io
-
-sed -i -e 's/\(<org-agenda-calendar-event>\).*\(<\/org-agenda-calendar-event>\)/<org-agenda-calendar-event>---<\/org-agenda-calendar-event>/g' index.html
-
 git add --all
 git commit -m "update cal"
 git push
